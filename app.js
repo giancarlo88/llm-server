25b6ac532f2381bb50a631adefffcbd661a5d3e6
Giancarlo Galliani Pecchia <giancarlo.pecchia@arcadiagroup.co.uk>
2018-04-21 16:45:04 +0100

    Revert access-control-allow-origin hearder to environment variable

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 var allowCrossDomain = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', `${process.env.HOST}`)
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   res.header('Access-Control-Allow-Headers', 'Content-Type')
 
